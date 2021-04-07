edafb52f2ab9d8cea966afb8071d0ca76d9142b3
“Alan <alan.hillgemann@anz.com>
2021-04-07 10:21:39 +1000

    feat: handle DELETE requests for questions by question ID

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -73,13 +73,19 @@ def create_app(test_config=None):
             abort(404)
 
 
-    '''
-    @TODO:
-    Create an endpoint to DELETE question using a question ID.
+    # Handle DELETE requests for questions by question ID
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        question = Question.query.get(question_id)
+        if question is not None:
+            question.delete()
+            return jsonify({
+                'success': True,
+                'deleted': question_id
+            })
+        else:
+            abort(404)
 
-    TEST: When you click the trash icon next to a question, the question will be removed.
-    This removal will persist in the database and when you refresh the page.
-    '''
 
     '''
     @TODO:
